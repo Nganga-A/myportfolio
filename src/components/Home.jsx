1ec65a19b5d57f4d6a52495c8e5dc5f7199a032e
Abed Nganga <ngangaabed@gmail.com>
2023-12-14 22:15:28 +0300

    Home component initial structure

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React from 'react';
+import HeroImage from '../assets/heroImage.png';
+import { HiMiniArrowRight } from "react-icons/hi2";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
+        <div>
+            <div>
+                <h2>I'm a Full Stack Developer</h2>
+                <p>My expertise encompasses a comprehensive understanding
+of software engineering principles and methodologies, which I adeptly apply to surmount complex
+problems.I thrive as a collaborative team-player, excelling in dynamic environments and derive genuine
+passion from tackling intricate technical challenges.</p>
+
+                <div>
+                    <button>
+                        Portfolio
+                        <span> <HiMiniArrowRight /> </span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
