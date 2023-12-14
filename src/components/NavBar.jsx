4bbfe1f8f64bdb7318e12a42a2ddfeb27ede5eb5
Abed Nganga <ngangaabed@gmail.com>
2023-12-14 14:49:29 +0300

    Navbar initial structure

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,15 @@ import{FaBars, FaTimes } from 'react-icons/fa';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed'>
+
+      {/* Logo */}
+      <div>
+        <h1 className='text-5xl font-signature ml-5'>Abed</h1>
+      </div>
+
+      {/* links */}
+    </div>
   )
 }
 
