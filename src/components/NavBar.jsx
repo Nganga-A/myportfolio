400cf2038beefc1a643658cf6edf019e42d897ba
Abed Nganga <ngangaabed@gmail.com>
2023-12-14 15:02:55 +0300

    Added navbar menu items

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
 import{FaBars, FaTimes } from 'react-icons/fa';
 
+
+
 const NavBar = () => {
+
+  const links = [
+    {
+      id: 1,
+      link: 'Home',
+    },
+    {
+      id: 2,
+      link: 'About',
+    },
+    {
+      id: 3,
+      link: 'Portfolio',
+    },
+    {
+      id: 4,
+      link: 'Experience',
+    },
+    {
+      id: 5,
+      link: 'Contact',
+    },
+  ]
+
   return (
     <div className='flex justify-between items-center w-full h-20 text-white bg-black px-4 fixed'>
 
@@ -11,6 +37,18 @@ const NavBar = () => {
       </div>
 
       {/* links */}
+      <ul className='flex'>
+        {links.map(({id, link}) => (
+          <li
+            key={id}
+            className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200' >
+              {link}
+          </li>
+        ))}
+        
+
+      </ul>
+
     </div>
   )
 }
