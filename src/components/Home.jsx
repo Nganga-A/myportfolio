273cce624bea8074b0558bf73d66b65047df2410
Abed Nganga <ngangaabed@gmail.com>
2023-12-14 23:17:00 +0300

    Styling header

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,11 +4,11 @@ import { HiMiniArrowRight } from "react-icons/hi2";
 
 const Home = () => {
   return (
-    <div name="home" className='h-screen w-full bg-gradient-to-b from-black to-gray-800'>
-        <div>
-            <div>
-                <h2>I'm a Full Stack Developer</h2>
-                <p>My expertise encompasses a comprehensive understanding
+    <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
+            <div className='flex flex-col justify-center h-full'>
+                <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
+                <p className='text-gray-500 py'>My expertise encompasses a comprehensive understanding
 of software engineering principles and methodologies, which I adeptly apply to surmount complex
 problems.I thrive as a collaborative team-player, excelling in dynamic environments and derive genuine
 passion from tackling intricate technical challenges.</p>
@@ -20,6 +20,11 @@ passion from tackling intricate technical challenges.</p>
                     </button>
                 </div>
             </div>
+
+            <div>
+                <img src={HeroImage} alt="myprofile" className='rounded-2xl mx-auto w-2/3 ' />
+            </div>
+
         </div>
     </div>
   )
