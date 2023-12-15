889e64762bf5015ee21023bd72b379ca0eaabef5
Abed Nganga <ngangaabed@gmail.com>
2023-12-15 15:33:25 +0300

    Initial About section structure

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+        <div>
+            <div>
+                <p>About</p>
+            </div>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus esse laboriosam nam obcaecati quas perferendis odit officiis rerum quia, nisi hic, commodi rem placeat cum, optio sint dolores molestiae corrupti temporibus velit tenetur? Adipisci perspiciatis nesciunt voluptas nostrum provident. Corrupti cumque fugiat culpa possimus explicabo suscipit eos, minus molestiae eaque!
+            </p>
+            <br/>
+            <p>
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Provident cum sit numquam impedit. Tenetur, omnis ducimus est nemo dolor similique fuga earum? Ipsa quasi quibusdam cumque odit ratione aut, fugiat porro voluptatibus recusandae, molestiae vitae consequuntur dolore! Delectus commodi consectetur quis repudiandae. Possimus ad sequi dolorem porro animi nobis! Architecto.
+            </p>
+        </div>
+    </div>
   )
 }
 
