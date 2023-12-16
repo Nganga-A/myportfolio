85b20e13584a320a86c9275a25a98ba77fbfc65f
Abed Nganga <ngangaabed@gmail.com>
2023-12-16 12:06:49 +0300

    Added imports

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+import Experience from "./components/Experience";
 
 function App() {
   return (
