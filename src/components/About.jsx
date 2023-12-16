a6a8b9226dd4dbe9535d05162e1c6d9ccf2f816f
Abed Nganga <ngangaabed@gmail.com>
2023-12-16 16:46:50 +0300

    Refined styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white'>
-        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'> 
+    <div name="about" className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white pt-10'>
+        <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full sm:pt-20 md:pt-10 lg:py-0 xl:py-0'> 
             <div className='pb-8'> 
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
