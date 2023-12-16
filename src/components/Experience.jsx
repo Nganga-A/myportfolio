db21d2e8d7ff5b80a5695dc1fa89565a327b6e3f
Abed Nganga <ngangaabed@gmail.com>
2023-12-16 12:27:40 +0300

    Card array

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,6 +10,57 @@ import tailwind from "../assets/tailwind.png";
 
 
 const Experience = () => {
+    const techs = [
+      {
+        id: 1,
+        src: html,
+        title: "HTML",
+        style: "shadow-orange-500",
+      },
+      {
+        id: 2,
+        src: css,
+        title: "CSS",
+        style: "shadow-blue-500",
+      },
+      {
+        id: 3,
+        src: javascript,
+        title: "JavaScript",
+        style: "shadow-yellow-500",
+      },
+      {
+        id: 4,
+        src: reactImage,
+        title: "React",
+        style: "shadow-blue-600",
+      },
+      {
+        id: 5,
+        src: tailwind,
+        title: "Tailwind",
+        style: "shadow-sky-400",
+      },
+      {
+        id: 6,
+        src: nextjs,
+        title: "Next JS",
+        style: "shadow-white",
+      },
+      {
+        id: 7,
+        src: graphql,
+        title: "GraphQL",
+        style: "shadow-pink-400",
+      },
+      {
+        id: 8,
+        src: github,
+        title: "GitHub",
+        style: "shadow-gray-400",
+      },
+    ];
+
   return (
     <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
         <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
