4a2fcabb1ab85e53b559633c4a7f88bcc9342ce6
Abed Nganga <ngangaabed@gmail.com>
2023-12-16 15:23:09 +0300

    Configured react link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import{FaBars, FaTimes } from 'react-icons/fa';
+import {Link} from  'react-scroll';
 
 
 
@@ -44,7 +45,9 @@ const NavBar = () => {
           <li
             key={id}
             className='px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200 hover:text-orange-400' >
-              {link}
+              <Link to={link.toLowerCase()} smooth duration={500}>
+                {link}
+              </Link>
           </li>
         ))}
       </ul>
@@ -60,7 +63,10 @@ const NavBar = () => {
             <li
             key={id}
             className='px-4 cursor-pointer capitalize py-6 text-4xl'>
-              {link}
+              <Link onClick={() => setNav(!nav)}
+               to={link.toLowerCase()} smooth duration={500}>
+                {link}
+              </Link>
             </li>
 
           ))}
