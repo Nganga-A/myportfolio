8ef17080e3b586c85fe517be684485b992fa0467
Abed Nganga <ngangaabed@gmail.com>
2023-12-16 14:18:04 +0300

    Contact form initial structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+        <div>
+            <div>
+                <p>Contact</p>
+                <p>Submit the form below to get in touch with me</p>
+            </div>
+            <div>
+                <form action="">
+                    <input type="text" name="name" placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
+                    <textarea className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' name="message" rows="10"></textarea>
+                    <button className='text-white bg-gradient-to-b from-cyan-800 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
+                </form>
+            </div>
+        </div>
+    </div>
   )
 }
 
