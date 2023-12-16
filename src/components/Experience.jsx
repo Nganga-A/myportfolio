4a20764fe6cc17fa362b159c7080e01b2c55bbf3
Abed Nganga <ngangaabed@gmail.com>
2023-12-16 12:25:50 +0300

    Initial card structure

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -11,7 +11,20 @@ import tailwind from "../assets/tailwind.png";
 
 const Experience = () => {
   return (
-    <div></div>
+    <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
+        <div className='max-w-screen-lg mx-auto p-4 flex flex-col justify-center w-full h-full text-white'>
+            <div>
+                <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Experience</p>
+                <p className='py-6'>These are the technologies i have worked this</p>
+            </div>
+            <div className='w-full gitd grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
+                <div className='shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
+                    <img src={html} className='w-28 mx-auto' alt='' />
+                    <p className='mt-4'>HTML5</p>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
