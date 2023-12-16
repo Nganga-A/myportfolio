c9ea41bf2a69a873202f21b07831beac8661b8ed
Abed Nganga <ngangaabed@gmail.com>
2023-12-16 17:35:49 +0300

    Added footer to appjs

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
 import Experience from "./components/Experience";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Portfolio />
       <Experience />
       <Contact />
+      <Footer />
     </div>
   );
 }
