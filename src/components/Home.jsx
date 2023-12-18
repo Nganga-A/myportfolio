872a42e7fb253f6b69b3f8c11a03e779ca1d94e4
Abed Nganga <ngangaabed@gmail.com>
2023-12-18 04:12:33 +0300

    Updated styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,14 +6,14 @@ import {Link} from "react-scroll";
 const Home = () => {
   return (
     <div name="home" className='h-screen w-full bg-gradient-to-b from-black via-black to-gray-800'>
-        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
-            <div className='flex flex-col justify-center h-full'>
+        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row p-0'>
+            <div className='flex flex-col justify-center h-full my-0'>
                 <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
                 <p className='text-gray-500 py-6 max-w-md text-xl'>Passionate about turning ideas into impactful solutions. Join me in this coding journey where creativity meets functionality and let's build something extraordinary together.</p>
 
                 <div>
                     <Link to="portfolio" smooth duration={500}
-                     className='groupt ext-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 duration-300'>
+                     className='group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer hover:scale-105 duration-300'>
                         Portfolio
                         <span className='group-hover:rotate-90 duration-300'>
                             <HiMiniArrowRight size={20} className='ml-1'/>
