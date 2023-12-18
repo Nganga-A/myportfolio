4cd20f961839d5623cccf9d5b222cb1800d52de5
Abed Nganga <ngangaabed@gmail.com>
2023-12-18 04:16:54 +0300

    Removed unnecessary padding

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div name="about" className='w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white pt-40'>
+    <div name="about" className='w-full h-auto bg-gradient-to-b from-gray-800 to-black text-white pt-40 my-1'>
         <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full sm:pt-20 md:pt-10 lg:py-0 xl:py-0'> 
-            <div className='pb-8'> 
+            <div className='pb-0'> 
                 <p className='text-4xl font-bold inline border-b-4 border-gray-500'>About</p>
             </div>
             <p className='text-xl mt-10'>
